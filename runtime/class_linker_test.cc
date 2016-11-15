6e46b8eb9c285ac1bdbdcd8fd2ce2ba37c8ca637
Mathieu Chartier <mathieuc@google.com>
2016-11-15 15:51:31 -0800

    Change iftable to never be null
    Simplifies code generation by removing a null check. The null case
    is rare.
    
    Ritzperf code size: 13107624 -> 13095336
    
    Bug: 32577579
    
    Test: test-art-host, run ritzperf both with CC
    
    Change-Id: I2b31e800867112869d7f0643e16c08826296979e

--- runtime/class_linker_test.cc
+++ runtime/class_linker_test.cc
@@ -101,7 +101,7 @@ class ClassLinkerTest : public CommonRuntimeTest {
     EXPECT_EQ(0U, primitive->NumDirectInterfaces());
     EXPECT_FALSE(primitive->HasVTable());
     EXPECT_EQ(0, primitive->GetIfTableCount());
-    EXPECT_TRUE(primitive->GetIfTable() == nullptr);
+    EXPECT_TRUE(primitive->GetIfTable() != nullptr);
     EXPECT_EQ(kAccPublic | kAccFinal | kAccAbstract, primitive->GetAccessFlags());
   }
 
