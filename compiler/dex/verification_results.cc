1264cf322f2fd6af3916fb066b8091a94602b9d7
Nicolas Geoffray <ngeoffray@google.com>
2016-11-25 15:56:12 +0000

    Create empty VerifiedMethod after vdex verification.
    The compiler and quicken require the existence of a
    VerifiedMethod for compiling a method.
    
    This fixes the regression of not doing any compilation when
    passed --input-vdex.
    
    Test: manual, but working on a run-test.
    
    Change-Id: Ie65578eadd09099df1c1a403d96c15e5da78a901

--- compiler/dex/verification_results.cc
+++ compiler/dex/verification_results.cc
@@ -98,6 +98,13 @@ const VerifiedMethod* VerificationResults::GetVerifiedMethod(MethodReference ref
   return (it != verified_methods_.end()) ? it->second : nullptr;
 }
 
+void VerificationResults::CreateVerifiedMethodFor(MethodReference ref) {
+  DexFileMethodArray* array = GetMethodArray(ref.dex_file);
+  DCHECK(array != nullptr);
+  (*array)[ref.dex_method_index].StoreSequentiallyConsistent(
+      new VerifiedMethod(/* encountered_error_types */ 0, /* has_runtime_throw */ false));
+}
+
 void VerificationResults::AddRejectedClass(ClassReference ref) {
   {
     WriterMutexLock mu(Thread::Current(), rejected_classes_lock_);
