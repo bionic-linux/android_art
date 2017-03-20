b03ee4731b8aae74f4c6a63d2cde30e94b95a32a
Vladimir Marko <vmarko@google.com>
2017-03-20 19:23:21 +0000

    Revert "Partial revert of "Reduce using ArtMethod's dex_cache_resolved_types_.""
    Should be fixed by
        https://android-review.googlesource.com/352330
    (diff PS2 PS4).
    
    Bug:34452336
    
    This reverts commit 4c04117b42f743cdd63ce8107720c8de8103da5e.
    
    Change-Id: Ifbf17a49de4010af7f10ae13428026242a5cb936

--- runtime/reflection.cc
+++ runtime/reflection.cc
@@ -216,54 +216,43 @@ class ArgArray {
   }
 
   bool BuildArgArrayFromObjectArray(ObjPtr<mirror::Object> receiver,
-                                    ObjPtr<mirror::ObjectArray<mirror::Object>> raw_args,
-                                    ArtMethod* m,
-                                    Thread* self)
+                                    ObjPtr<mirror::ObjectArray<mirror::Object>> args,
+                                    ArtMethod* m)
       REQUIRES_SHARED(Locks::mutator_lock_) {
     const DexFile::TypeList* classes = m->GetParameterTypeList();
     // Set receiver if non-null (method is not static)
     if (receiver != nullptr) {
       Append(receiver);
     }
-    StackHandleScope<2> hs(self);
-    MutableHandle<mirror::Object> arg(hs.NewHandle<mirror::Object>(nullptr));
-    Handle<mirror::ObjectArray<mirror::Object>> args(
-        hs.NewHandle<mirror::ObjectArray<mirror::Object>>(raw_args));
     for (size_t i = 1, args_offset = 0; i < shorty_len_; ++i, ++args_offset) {
-      arg.Assign(args->Get(args_offset));
-      if (((shorty_[i] == 'L') && (arg.Get() != nullptr)) ||
-          ((arg.Get() == nullptr && shorty_[i] != 'L'))) {
-        // TODO: The method's parameter's type must have been previously resolved, yet
-        // we've seen cases where it's not b/34440020.
+      ObjPtr<mirror::Object> arg(args->Get(args_offset));
+      if (((shorty_[i] == 'L') && (arg != nullptr)) || ((arg == nullptr && shorty_[i] != 'L'))) {
+        // Note: The method's parameter's type must have been previously resolved.
         ObjPtr<mirror::Class> dst_class(
             m->GetClassFromTypeIndex(classes->GetTypeItem(args_offset).type_idx_,
-                                     true /* resolve */));
-        if (dst_class.Ptr() == nullptr) {
-          CHECK(self->IsExceptionPending());
-          return false;
-        }
-        if (UNLIKELY(arg.Get() == nullptr || !arg->InstanceOf(dst_class))) {
+                                     false /* resolve */));
+        DCHECK(dst_class != nullptr) << m->PrettyMethod() << " arg #" << i;
+        if (UNLIKELY(arg == nullptr || !arg->InstanceOf(dst_class))) {
           ThrowIllegalArgumentException(
               StringPrintf("method %s argument %zd has type %s, got %s",
                   m->PrettyMethod(false).c_str(),
                   args_offset + 1,  // Humans don't count from 0.
                   mirror::Class::PrettyDescriptor(dst_class).c_str(),
-                  mirror::Object::PrettyTypeOf(arg.Get()).c_str()).c_str());
+                  mirror::Object::PrettyTypeOf(arg).c_str()).c_str());
           return false;
         }
       }
 
 #define DO_FIRST_ARG(match_descriptor, get_fn, append) { \
-          if (LIKELY(arg.Get() != nullptr && \
-              arg->GetClass()->DescriptorEquals(match_descriptor))) { \
+          if (LIKELY(arg != nullptr && arg->GetClass()->DescriptorEquals(match_descriptor))) { \
             ArtField* primitive_field = arg->GetClass()->GetInstanceField(0); \
-            append(primitive_field-> get_fn(arg.Get()));
+            append(primitive_field-> get_fn(arg));
 
 #define DO_ARG(match_descriptor, get_fn, append) \
-          } else if (LIKELY(arg.Get() != nullptr && \
+          } else if (LIKELY(arg != nullptr && \
                             arg->GetClass<>()->DescriptorEquals(match_descriptor))) { \
             ArtField* primitive_field = arg->GetClass()->GetInstanceField(0); \
-            append(primitive_field-> get_fn(arg.Get()));
+            append(primitive_field-> get_fn(arg));
 
 #define DO_FAIL(expected) \
           } else { \
@@ -277,14 +266,14 @@ class ArgArray {
                       ArtMethod::PrettyMethod(m, false).c_str(), \
                       args_offset + 1, \
                       expected, \
-                      mirror::Object::PrettyTypeOf(arg.Get()).c_str()).c_str()); \
+                      mirror::Object::PrettyTypeOf(arg).c_str()).c_str()); \
             } \
             return false; \
           } }
 
       switch (shorty_[i]) {
         case 'L':
-          Append(arg.Get());
+          Append(arg);
           break;
         case 'Z':
           DO_FIRST_ARG("Ljava/lang/Boolean;", GetBoolean, Append)
@@ -657,7 +646,7 @@ jobject InvokeMethod(const ScopedObjectAccessAlreadyRunnable& soa, jobject javaM
   uint32_t shorty_len = 0;
   const char* shorty = np_method->GetShorty(&shorty_len);
   ArgArray arg_array(shorty, shorty_len);
-  if (!arg_array.BuildArgArrayFromObjectArray(receiver, objects, np_method, soa.Self())) {
+  if (!arg_array.BuildArgArrayFromObjectArray(receiver, objects, np_method)) {
     CHECK(soa.Self()->IsExceptionPending());
     return nullptr;
   }
