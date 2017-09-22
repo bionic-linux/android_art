4e266fb83f0a8b27d28f0947392af9b8f9808227
Richard Uhler <ruhler@google.com>
2017-09-22 11:47:55 +0000

    Revert "ahat: Expand test coverage using static heap dumps."
    Breaks mac build.
    This reverts commit 72f0ec717ce1ee13108d65af83fc99f8e507d75d.
    
    Change-Id: Ie80eb298c554c09cd02958d8b5cb5ae250f9602e

--- tools/ahat/src/heapdump/AhatClassInstance.java
+++ tools/ahat/src/heapdump/AhatClassInstance.java
@@ -123,7 +123,7 @@ public class AhatClassInstance extends AhatInstance {
     }
 
     Value value = getField("value");
-    if (value == null || !value.isAhatInstance()) {
+    if (!value.isAhatInstance()) {
       return null;
     }
 
--- tools/ahat/src/heapdump/AhatPlaceHolderClassObj.java
+++ tools/ahat/src/heapdump/AhatPlaceHolderClassObj.java
@@ -29,10 +29,6 @@ public class AhatPlaceHolderClassObj extends AhatClassObj {
     baseline.setBaseline(this);
   }
 
-  @Override public Size getSize() {
-    return Size.ZERO;
-  }
-
   @Override public Size getRetainedSize(AhatHeap heap) {
     return Size.ZERO;
   }
