82833e4a45579c178186866a65662c729b2f944a
Alan Leung <acleung@google.com>
2017-12-08 14:08:25 -0800

    Update java.lang.String class size.
    In standalone desugar, one of the implemented interface CharSequence has
    two lambdas:
    
    lambda$chars$0$CharSequence
    lambda$codePoints$1$CharSequence
    
    which are virtual functions.
    
    In D8 Desugar, both of them are now direct methods.
    
    Bug: 69477285
    Test: USE_D8_DESUGAR=true m
    Change-Id: I94aaa42e86bd104fc86fa14d1eb45b2e906da5a9

--- runtime/mirror/string-inl.h
+++ runtime/mirror/string-inl.h
@@ -35,7 +35,7 @@ namespace art {
 namespace mirror {
 
 inline uint32_t String::ClassSize(PointerSize pointer_size) {
-  uint32_t vtable_entries = Object::kVTableLength + 56;
+  uint32_t vtable_entries = Object::kVTableLength + 54;
   return Class::ComputeClassSize(true, vtable_entries, 0, 0, 0, 1, 2, pointer_size);
 }
 
