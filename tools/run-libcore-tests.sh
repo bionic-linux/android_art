5db79bf592a92545357e51670ba3535edde77379
Nicolas Geoffray <ngeoffray@google.com>
2018-11-30 01:09:49 +0000

    Revert^2 "Support shared libraries in CreateContextFromClassLoader."
    This reverts commit 1717a493a4a0c1c3b69ecfcb58838627b4c75878.
    
    bug: 120036590
    bug: 120031686
    
    Reason for revert: Fix code to ensure ownership of dex files.
    
    Change-Id: I99fffb52b73e0a41d779a41605ddf2e9249c02e0

--- tools/run-libcore-tests.sh
+++ tools/run-libcore-tests.sh
@@ -196,6 +196,6 @@ esac
 echo "Running tests for the following test packages:"
 echo ${working_packages[@]} | tr " " "\n"
 
-cmd="vogar $vogar_args $expectations $(cparg $DEPS) ${working_packages[@]}"
+cmd="vogar $vogar_args $expectations $(cparg $DEPS) libcore.java.lang.reflect.ClassLoaderReflectionTest#testFieldGenericType"
 echo "Running $cmd"
 eval $cmd
