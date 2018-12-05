792659209af00bf5435f019065ef6bb16c0ddb7e
Hans Boehm <hboehm@google.com>
2018-12-05 11:11:33 -0800

    Selectively allow dead reference elimination
    Allow dead reference elimination in methods not containing
    @ReachabilitySensitive accesses or calls, when the class is marked
    @DeadReferenceSafe.
    
    Add 1339-dead-reference-safe to aggressively check that everything
    works as intended.
    
    Bug: 111453875
    
    Test: Seems to build. Test fails.
    
    Detect ReachabilitySensitive annotations.
    
    Change-Id: I70c20431fdbcfcfd2692b2255d12ad59e37cb669

---
+++ test/1339-dead-reference-safe/src/DeadReferenceSafeTest.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import dalvik.annotation.optimization.DeadReferenceSafe;
+import dalvik.annotation.optimization.ReachabilitySensitive;
+import java.lang.ref.Reference;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@DeadReferenceSafe
+public final class DeadReferenceSafeTest implements Runnable {
+  AtomicInteger nFinalized = new AtomicInteger(0);
+  boolean realTest = false;
+  final int WARMUP_ITERS = 2000;
+  final int INNER_ITERS = 10;
+  static int n;
+
+  // ??? FIXME: This shouldn't be needed. We should proagate @DeadReferenceSafe to inner classes.
+  @DeadReferenceSafe
+  class Finalizable {
+    final int m = 1;
+    protected void finalize() {
+      nFinalized.incrementAndGet();
+    }
+    int doLittle() {
+      n++;
+      return m;
+    }
+  }
+
+  @DeadReferenceSafe
+  class SensitiveFinalizable {
+    @ReachabilitySensitive
+    final int m = 1;
+    protected void finalize() {
+      nFinalized.incrementAndGet();
+    }
+    int doLittle() {
+      n++;
+      return m;
+    }
+  }
+
+  Finalizable aFinalizable = new Finalizable();
+  SensitiveFinalizable aSensitiveFinalizable = new SensitiveFinalizable();
+
+  private void loop() {
+    // Each loop allocates INNER_ITERS Finalizable objects, and calls doLittle()
+    // INNER_ITERS - 1 times.
+    Finalizable x = aFinalizable;
+    for (int i = 0; i < INNER_ITERS; ++i) {
+      if (i == 5 && realTest) {
+        // At the start of iteration n, we've allocated n objects.
+        Runtime.getRuntime().gc();
+        System.runFinalization();
+        // With dead reference elimination, x should not be traced by the GC here.
+        // Thus all 5 prior objects should have been found unreachable and finalized.
+        int finalizedCount = nFinalized.get();
+        System.out.println("Loop finalized " + finalizedCount + " objects");
+        if (finalizedCount != 5) {
+          System.out.println("Dead reference erroneously retained!");
+        }
+      }
+      // Expected to be inlined and not to keep x live.
+      x.doLittle();
+      x = new Finalizable();
+    }
+  }
+
+  private void sensitiveLoop() {
+    SensitiveFinalizable x = aSensitiveFinalizable;
+    for (int i = 0; i < INNER_ITERS; ++i) {
+      if (i == 5 && realTest) {
+        Runtime.getRuntime().gc();
+        System.runFinalization();
+        // With dead reference elimination, x should not be traced by the GC here.
+        // Thus all 5 prior objects should have been found unreachable and finalized.
+        int finalizedCount = nFinalized.get();
+        System.out.println("SensitiveLoop finalized " + finalizedCount + " objects");
+        if (finalizedCount != 4) {
+          System.out.println("ReachabilitySensitive access ignored!");
+        }
+      }
+      // Expected to be inlined. Should keep x live, since it accesses ReachabilitySensitive field.
+      x.doLittle();
+      x = new SensitiveFinalizable();
+    }
+  }
+
+  private void fencedLoop() {
+    Finalizable x = aFinalizable;
+    for (int i = 0; i < INNER_ITERS; ++i) {
+      if (i == 5 && realTest) {
+        Runtime.getRuntime().gc();
+        System.runFinalization();
+        // With dead reference elimination, x should not be traced by the GC here.
+        // Thus all 5 prior objects should have been found unreachable and finalized.
+        int finalizedCount = nFinalized.get();
+        System.out.println("FencedLoop finalized " + finalizedCount + " objects");
+        if (finalizedCount != 4) {
+          System.out.println("ReachabilityFence ignored!");
+        }
+      }
+      // Expected to be inlined and not to keep x live.
+      x.doLittle();
+      // But this should.
+      Reference.reachabilityFence(x);
+      x = new Finalizable();
+    }
+  }
+
+  private void reset(int expected_count) {
+    Runtime.getRuntime().gc();
+    System.runFinalization();
+    if (nFinalized.get() != expected_count) {
+      System.out.println("Wrong number of finalized objects:" + nFinalized.get());
+    }
+    nFinalized.set(0);
+  }
+
+  private void mySleep(long n) {
+    try {
+      Thread.sleep(n);
+    } catch(InterruptedException e) {
+      throw new AssertionError("Interrupted");
+    }
+  }
+
+  public void run() {
+    // Strongly encourage loop() etc. to be compiled.
+    for (int i = 0; i < WARMUP_ITERS; ++i) {
+      loop();
+    }
+    mySleep(50);
+    for (int i = 0; i < WARMUP_ITERS; ++i) {
+      sensitiveLoop();
+    }
+    mySleep(50);
+    for (int i = 0; i < WARMUP_ITERS; ++i) {
+      fencedLoop();
+    }
+    mySleep(300);
+    realTest = true;
+
+    reset(3 * WARMUP_ITERS * INNER_ITERS);
+    loop();
+
+    reset(INNER_ITERS);
+    sensitiveLoop();
+
+    reset(INNER_ITERS);
+    fencedLoop();
+
+    if (n != 3 * (WARMUP_ITERS + 1) * INNER_ITERS) {
+      System.out.println("doLittle() execution count is wrong: " + n);
+    }
+    reset(INNER_ITERS);
+  }
+}
---
+++ test/1339-dead-reference-safe/src/DeadReferenceUnsafeTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import dalvik.annotation.optimization.DeadReferenceSafe;
+import dalvik.annotation.optimization.ReachabilitySensitive;
+import java.util.concurrent.atomic.AtomicInteger;
+
+// A subset of DeadReferenceSafeTest, but without the annotation. It should behave
+// differently.
+
+public final class DeadReferenceUnsafeTest implements Runnable {
+  AtomicInteger nFinalized = new AtomicInteger(0);
+  boolean realTest = false;
+  final int WARMUP_ITERS = 2000;
+  final int INNER_ITERS = 10;
+  static int n;
+
+  class Finalizable {
+    final int m = 1;
+    protected void finalize() {
+      nFinalized.incrementAndGet();
+    }
+    int doLittle() {
+      n++;
+      return m;
+    }
+  }
+
+  Finalizable aFinalizable = new Finalizable();
+
+  private void loop() {
+    // Each loop allocates INNER_ITERS Finalizable objects, and calls doLittle()
+    // INNER_ITERS times.
+    Finalizable x = aFinalizable;
+    for (int i = 0; i < INNER_ITERS; ++i) {
+      // At the start of iteration n, we've allocated n objects.
+      if (i == 5 && realTest) {
+        Runtime.getRuntime().gc();
+        System.runFinalization();
+        // With dead reference elimination, x should not be traced by the GC here.
+        // Thus all 5 prior objects should have been found unreachable and finalized.
+        int finalizedCount = nFinalized.get();
+        System.out.println("Finalized " + finalizedCount + " objects");
+        if (finalizedCount != 4) {
+          System.out.println("Dead reference should be retained by default!");
+        }
+      }
+      // Expected to be inlined and not to keep x live. But we shouldn't eliminate dead
+      // references anyway.
+      x.doLittle();
+      x = new Finalizable();
+    }
+  }
+
+  private void reset(int expected_count) {
+    Runtime.getRuntime().gc();
+    System.runFinalization();
+    if (nFinalized.get() != expected_count) {
+      System.out.println("DeadReferenceUnsafeTest: Wrong number of finalized objects:"
+                         + nFinalized.get());
+    }
+    nFinalized.set(0);
+  }
+
+  public void run() {
+    // Strongly encourage loop() etc. to be compiled.
+    for (int i = 0; i < WARMUP_ITERS; ++i) {
+      loop();
+    }
+    try {
+      Thread.sleep(300);
+    } catch(InterruptedException e) {
+      throw new AssertionError("Interrupted");
+    }
+    realTest = true;
+
+    reset(WARMUP_ITERS * INNER_ITERS);
+    loop();
+
+    if (n != (WARMUP_ITERS + 1) * INNER_ITERS) {
+      System.out.println("DeadReferenceUnsafeTest: Finalizable.n wrong: " + n);
+    }
+    reset(INNER_ITERS);
+  }
+}
