068d83cbd6ad3df0d1cf6f129e6b937654644099
Vladimir Marko <vmarko@google.com>
2018-12-13 13:32:13 +0000

    Add apache-xml to boot class path for tests.
    And partially revert
        7a85e70b2bf646d1d7a226fbb4e7fafb66871dd5
    and fully revert
        d4291d68c5dffd992ed8d46d0785d24a5da80026
    to re-enable tests.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: m test-art-target-gtest
    Test: testrunner.py --target --optimizing
    Test: run-libcore-tests --mode=host --variant=X64
    Test: run-jdwp-tests --mode=host --variant=X64
    Test: run-libcore-tests --mode=target --variant=X64
    Test: run-jdwp-tests --mode=target --variant=X64
    Bug: 120526172
    Change-Id: I5af4853cae84bfc4807d98668b2da2fd9c5fc22e

--- libartbase/base/common_art_test.cc
+++ libartbase/base/common_art_test.cc
@@ -339,6 +339,7 @@ std::vector<std::string> CommonArtTestImpl::GetLibCoreDexFileNames() {
       "core-simple",
       "okhttp",
       "bouncycastle",
+      "apache-xml",
       // Additional modules.
       "conscrypt",
   };
