e89fe36a49c73fdc52576f6221e4cf829dbab6a3
David Brazdil <dbrazdil@google.com>
2018-12-13 17:00:06 +0000

    hiddenapi: Produce CSV for `list` command
    The `hiddenapi` build tool is used for generating a list of all public
    and private API. The output is currently two text files, each with
    a list of API signatures belonging to the corresponding API set
    (public/private). Refactor the code to produce a single CSV file
    instead in anticipation of other categories of APIs, namely
    @CorePlatformApi.
    
    Also change class2greylist, which is a consumer of the public API text
    file, to parse and filter the CSV file instead.
    
    Bug: 119068555
    Test: compiles, hiddenapi-flags.csv unchanged
    Change-Id: I4ac9d96c0d10a87795c6a779f0231269c99959a3

--- tools/class2greylist/src/com/android/class2greylist/Class2Greylist.java
+++ tools/class2greylist/src/com/android/class2greylist/Class2Greylist.java
@@ -34,10 +34,12 @@ import org.apache.commons.cli.ParseException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Build time tool for extracting a list of members from jar files that have the @UsedByApps
@@ -67,7 +69,6 @@ public class Class2Greylist {
     }
 
     private final Status mStatus;
-    private final String mPublicApiListFile;
     private final String mCsvFlagsFile;
     private final String mCsvMetadataFile;
     private final String[] mJarFiles;
@@ -77,10 +78,11 @@ public class Class2Greylist {
     public static void main(String[] args) {
         Options options = new Options();
         options.addOption(OptionBuilder
-                .withLongOpt("public-api-list")
+                .withLongOpt("stub-api-flags")
                 .hasArgs(1)
-                .withDescription("Public API list file. Used to de-dupe bridge methods.")
-                .create("p"));
+                .withDescription("CSV file with API flags generated from public API stubs. " +
+                        "Used to de-dupe bridge methods.")
+                .create("s"));
         options.addOption(OptionBuilder
                 .withLongOpt("write-flags-csv")
                 .hasArgs(1)
@@ -139,7 +141,7 @@ public class Class2Greylist {
             try {
                 Class2Greylist c2gl = new Class2Greylist(
                         status,
-                        cmd.getOptionValue('p', null),
+                        cmd.getOptionValue('s', null),
                         cmd.getOptionValue('w', null),
                         cmd.getOptionValue('c', null),
                         jarFiles);
@@ -158,11 +160,10 @@ public class Class2Greylist {
     }
 
     @VisibleForTesting
-    Class2Greylist(Status status, String publicApiListFile, String csvFlagsFile,
+    Class2Greylist(Status status, String stubApiFlagsFile, String csvFlagsFile,
             String csvMetadataFile, String[] jarFiles)
             throws IOException {
         mStatus = status;
-        mPublicApiListFile = publicApiListFile;
         mCsvFlagsFile = csvFlagsFile;
         mCsvMetadataFile = csvMetadataFile;
         mJarFiles = jarFiles;
@@ -172,9 +173,13 @@ public class Class2Greylist {
             mOutput = new HiddenapiFlagsWriter(mCsvFlagsFile);
         }
 
-        if (mPublicApiListFile != null) {
-            mPublicApis = Sets.newHashSet(
-                    Files.readLines(new File(mPublicApiListFile), Charset.forName("UTF-8")));
+        if (stubApiFlagsFile != null) {
+            mPublicApis =
+                    Files.readLines(new File(stubApiFlagsFile), Charset.forName("UTF-8")).stream()
+                        .map(s -> s.split(","))
+                        .filter(s -> Arrays.asList(s).contains(FLAG_WHITELIST))
+                        .map(s -> s[0])
+                        .collect(Collectors.toSet());
         } else {
             mPublicApis = Collections.emptySet();
         }
