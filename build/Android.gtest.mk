0bbc22590208ebb7d044d8ae437f6a1a14ca8c1b
Vladimir Marko <vmarko@google.com>
2018-12-13 13:32:13 +0000

    Add apache-xml to boot class path for tests.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: m test-art-target-gtest
    Test: testrunner.py --target --optimizing
    Test: run-libcore-tests --mode=host --variant=X64
    Test: run-jdwp-tests --mode=host --variant=X64
    Test: run-libcore-tests --mode=target --variant=X64
    Test: run-jdwp-tests --mode=target --variant=X64
    Bug: 120526172
    Change-Id: I5af4853cae84bfc4807d98668b2da2fd9c5fc22e

--- build/Android.gtest.mk
+++ build/Android.gtest.mk
@@ -455,9 +455,10 @@ define define-art-gtest-rule-target
     $$(TARGET_OUT_JAVA_LIBRARIES)/core-libart-testdex.jar \
     $$(TARGET_OUT_JAVA_LIBRARIES)/core-oj-testdex.jar \
     $$(TARGET_OUT_JAVA_LIBRARIES)/core-simple-testdex.jar \
-    $$(TARGET_OUT_JAVA_LIBRARIES)/conscrypt-testdex.jar \
     $$(TARGET_OUT_JAVA_LIBRARIES)/okhttp-testdex.jar \
-    $$(TARGET_OUT_JAVA_LIBRARIES)/bouncycastle-testdex.jar
+    $$(TARGET_OUT_JAVA_LIBRARIES)/bouncycastle-testdex.jar \
+    $$(TARGET_OUT_JAVA_LIBRARIES)/apache-xml-testdex.jar \
+    $$(TARGET_OUT_JAVA_LIBRARIES)/conscrypt-testdex.jar
 
   ART_TEST_TARGET_GTEST_DEPENDENCIES += $$(gtest_deps)
 
