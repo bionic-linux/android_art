85614986f8778badf8491083f7cd6f058d8ad557
David Brazdil <dbrazdil@google.com>
2018-12-14 14:36:15 +0000

    hiddenapi: Support 'core-platform-api' flag
    Add support for parsing @CorePlatformApi stubs and encoding it in
    hiddenapi dex flags of the corresponding fields/methods.
    
    (1) The CL refactors hiddenapi::ApiList class to store a second value:
    a bit vector of "domain API" flags. These are intended for encoding
    membership in a set of API stubs only available to certain callers,
    e.g. @CorePlatformApi when platform code calls core platform or
    @TestApi for CTS tests.
    
    (2) Parse @CorePlatformApi stubs and set domain flags for its members.
    
    (3) Parse the flags at runtime and set kAccCorePlatformApi access flag
    on the corresponding ArtField/ArtMethod objects.
    
    (4) Fix a bug which meant that DCHECK in SetIntrinsic would always pass.
    
    Bug: 119068555
    Test: m appcompat
    Test: dexlayout -b <core-oj jar> | grep 'CORE-PLATFORM-API'
    Change-Id: Idbfa6d3af7459258a5a0b6da7c03c037a577eb75

--- libartbase/base/hiddenapi_flags.h
+++ libartbase/base/hiddenapi_flags.h
@@ -52,15 +52,20 @@ class ApiList {
     kMaxValue =      kGreylistMaxP,
   };
 
-  static constexpr const char* kNames[] = {
+  enum class DomainApi : IntValueType {
+    kCorePlatformApi = 0,
+
+    kMinValue =        kCorePlatformApi,
+    kMaxValue =        kCorePlatformApi,
+  };
+
+  static constexpr const char* kApiListNames[] = {
     "whitelist",
     "greylist",
     "blacklist",
     "greylist-max-o",
     "greylist-max-p",
   };
-
-  static constexpr const char* kInvalidName = "invalid";
 
   static constexpr SdkVersion kMaxSdkVersions[] {
     /* whitelist */ SdkVersion::kMax,
@@ -70,69 +75,155 @@ class ApiList {
     /* greylist-max-p */ SdkVersion::kP,
   };
 
-  static ApiList MinValue() { return ApiList(Value::kMinValue); }
-  static ApiList MaxValue() { return ApiList(Value::kMaxValue); }
+  static constexpr const char* kDomainApiNames[] = {
+    "core-platform-api",
+  };
 
-  explicit ApiList(Value value) : value_(value) {}
+  static constexpr uint32_t GetDomainApiBitMask(DomainApi domain_api) {
+    return 1u << static_cast<IntValueType>(domain_api);
+  }
+
+  explicit ApiList(Value value) : value_(value), domain_apis_(0u) {}
+  explicit ApiList(DomainApi domain_api)
+      : value_(Value::kInvalid), domain_apis_(GetDomainApiBitMask(domain_api)) {}
+  ApiList(Value value, uint32_t domain_apis) : value_(value), domain_apis_(domain_apis) {}
+
+  bool HasValue() const { return value_ != Value::kInvalid; }
+  bool Hasdomain_apiFlags() const { return domain_apis_ != 0; }
 
   Value value_;
+  uint32_t domain_apis_;
+
+  static constexpr size_t kValueBitSize = 3;
+  static constexpr uint32_t kValueBitMask = (1u << kValueBitSize) - 1;
 
  public:
   ApiList() : ApiList(Value::kInvalid) {}
+  static ApiList Empty() { return ApiList(); }
 
   static ApiList Whitelist() { return ApiList(Value::kWhitelist); }
   static ApiList Greylist() { return ApiList(Value::kGreylist); }
   static ApiList Blacklist() { return ApiList(Value::kBlacklist); }
   static ApiList GreylistMaxO() { return ApiList(Value::kGreylistMaxO); }
   static ApiList GreylistMaxP() { return ApiList(Value::kGreylistMaxP); }
-  static ApiList Invalid() { return ApiList(Value::kInvalid); }
+
+  static ApiList CorePlatformApi() { return ApiList(DomainApi::kCorePlatformApi); }
 
   // Decodes ApiList from dex hiddenapi flags.
   static ApiList FromDexFlags(uint32_t dex_flags) {
-    if (MinValue().GetIntValue() <= dex_flags && dex_flags <= MaxValue().GetIntValue()) {
-      return ApiList(static_cast<Value>(dex_flags));
+    uint32_t dex_api_list = dex_flags & kValueBitMask;
+    uint32_t dex_domain_apis = dex_flags >> kValueBitSize;
+
+    if (dex_api_list < static_cast<IntValueType>(Value::kMinValue) ||
+        dex_api_list > static_cast<IntValueType>(Value::kMaxValue) ||
+        dex_domain_apis > GetDomainApiBitMask(DomainApi::kMaxValue)) {
+      return Empty();
     }
-    return Invalid();
+
+    return ApiList(static_cast<Value>(dex_api_list), dex_domain_apis);
+  }
+
+  uint32_t ToDexFlags() const {
+    CHECK(IsValid());
+    return static_cast<uint32_t>(static_cast<IntValueType>(value_)) |
+           (domain_apis_ << kValueBitSize);
   }
 
   // Decodes ApiList from its integer value.
   static ApiList FromIntValue(IntValueType int_value) {
-    if (MinValue().GetIntValue() <= int_value && int_value <= MaxValue().GetIntValue()) {
-      return ApiList(static_cast<Value>(int_value));
-    }
-    return Invalid();
+    CHECK_GE(int_value, static_cast<IntValueType>(Value::kMinValue));
+    CHECK_LE(int_value, static_cast<IntValueType>(Value::kMaxValue));
+    return ApiList(static_cast<Value>(int_value));
   }
 
   // Returns the ApiList with a given name.
   static ApiList FromName(const std::string& str) {
-    for (IntValueType i = MinValue().GetIntValue(); i <= MaxValue().GetIntValue(); i++) {
-      ApiList current = ApiList(static_cast<Value>(i));
-      if (str == current.GetName()) {
-        return current;
+    for (IntValueType i = static_cast<IntValueType>(Value::kMinValue);
+         i <= static_cast<IntValueType>(Value::kMaxValue);
+         i++) {
+      if (str == kApiListNames[i]) {
+        return ApiList(static_cast<Value>(i));
       }
     }
-    return Invalid();
+    for (IntValueType i = static_cast<IntValueType>(DomainApi::kMinValue);
+         i <= static_cast<IntValueType>(DomainApi::kMaxValue);
+         i++) {
+      if (str == kDomainApiNames[i]) {
+        return ApiList(static_cast<DomainApi>(i));
+      }
+    }
+    return Empty();
   }
 
-  bool operator==(const ApiList other) const { return value_ == other.value_; }
+  bool operator==(const ApiList other) const {
+    return (value_ == other.value_) && (domain_apis_ == other.domain_apis_);
+  }
+
   bool operator!=(const ApiList other) const { return !(*this == other); }
 
-  bool IsValid() const { return *this != Invalid(); }
+  const ApiList& operator|=(const ApiList other) {
+    if (other.HasValue()) {
+      if (HasValue()) {
+        CHECK(value_ == other.value_) << "Value conflict. Cannot assign two different values";
+      } else {
+        value_ = other.value_;
+      }
+    }
+    domain_apis_ |= other.domain_apis_;
+    return *this;
+  }
+
+  // Returns true whether the configuration is valid for runtime use.
+  bool IsValid() const { return HasValue(); }
+
+  // Returns true when no ApiList is specified and no domain_api flags either.
+  bool IsEmpty() const { return !HasValue() && !Hasdomain_apiFlags(); }
+
+  // Returns true if all flags set by `other` are also set in `this`.
+  bool Contains(const ApiList& other) const {
+    return (!other.HasValue() || (value_ == other.value_)) &&
+           ((other.domain_apis_ & domain_apis_) == other.domain_apis_);
+  }
 
   IntValueType GetIntValue() const {
     DCHECK(IsValid());
     return static_cast<IntValueType>(value_);
   }
 
-  const char* GetName() const { return IsValid() ? kNames[GetIntValue()]: kInvalidName; }
+  void Dump(std::ostream& os) const {
+    bool is_first = true;
+
+    if (HasValue()) {
+      os << kApiListNames[static_cast<IntValueType>(value_)];
+      is_first = false;
+    }
+
+    if (Hasdomain_apiFlags()) {
+      for (IntValueType i = static_cast<IntValueType>(DomainApi::kMinValue);
+           i <= static_cast<IntValueType>(DomainApi::kMaxValue);
+           i++) {
+        if ((domain_apis_ & GetDomainApiBitMask(static_cast<DomainApi>(i))) != 0) {
+          if (is_first) {
+            is_first = false;
+          } else {
+            os << ",";
+          }
+          os << kDomainApiNames[i];
+        }
+      }
+    }
+  }
 
   SdkVersion GetMaxAllowedSdkVersion() const { return kMaxSdkVersions[GetIntValue()]; }
 
   static constexpr size_t kValueCount = static_cast<size_t>(Value::kMaxValue) + 1;
+  static constexpr size_t kDomainApiCount = static_cast<size_t>(DomainApi::kMaxValue) + 1;
+
+  static_assert((1u << kValueBitSize) >= kValueCount, "Not enough bits to store Value");
 };
 
 inline std::ostream& operator<<(std::ostream& os, ApiList value) {
-  os << value.GetName();
+  value.Dump(os);
   return os;
 }
 
