0e006f3298f0b6c137e465b5875b9175875c0d7b
Orion Hodson <oth@google.com>
2019-01-21 16:01:30 +0000

    hiddenapi: check for CorePlatform domain
    Set Hidden API Domain to CorePlatform if dex file is in the Runtime
    Module.
    
    Bug: 119068555
    Test: 674-hiddenapi
    
    Change-Id: I24475098deb6f8d9d51d218ab1a8ee0caf01ae81

--- libartbase/base/file_utils.cc
+++ libartbase/base/file_utils.cc
@@ -278,6 +278,17 @@ std::string ReplaceFileExtension(const std::string& filename, const std::string&
   }
 }
 
+bool LocationIsOnRuntimeModule(const char* full_path) {
+  std::string error_msg;
+  std::string runtime_path = GetAndroidDirSafe("ANDROID_RUNTIME_ROOT",
+                                               "/apex/com.android.runtime",
+                                               &error_msg);
+  if (runtime_path.empty()) {
+    return false;
+  }
+  return android::base::StartsWith(full_path, runtime_path);
+}
+
 bool LocationIsOnSystem(const char* path) {
 #ifdef _WIN32
   UNUSED(path);
