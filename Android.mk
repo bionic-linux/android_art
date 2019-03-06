3cdf380a37e4392f81804492e0b49b036bcb5290
Roland Levillain <rpl@google.com>
2019-03-06 15:48:08 +0000

    Use flattened Runtime APEX contents in ART chroot-based device testing.
    Copy the contents of the flattened Debug Runtime Module
    directory (`/system/apex/com.android.runtime.debug`) to
    `/apex/com.android.runtime` within the chroot directory on the device.
    
    Avoid generating artifacts that are not expected in the `system`
    directory (`TARGET_OUT`) of a "normal" build.
    
    Adjust the chroot environment to have it use the system linker
    configuration of the built target ("guest system") and the linker
    configuration of the Runtime APEX, even if the linker configuration
    flavor of the "guest system" (e.g. legacy configuration) does not
    match the one of the "host system" (e.g. full-VNDK configuration).
    This is done by symlinking the configuration expected by the
    linker (governed by system properties of the "host system") to the
    configuration provided by the "guest system" (created according to the
    build target configuration).
    
    Test: ART chroot-based on-device testing using the master-art branch
    Test: ART chroot-based on-device testing using the master branch (and
          setting `TARGET_FLATTEN_APEX` set to `true` before building).
    Bug: 124425036
    Bug: 121117762
    Change-Id: Ied3f6ee8b2d68c3473fab864d1bbed9e88df59d3

--- Android.mk
+++ Android.mk
@@ -74,13 +74,7 @@ ART_HOST_DEPENDENCIES += $(ART_HOST_SHARED_LIBRARY_DEBUG_DEPENDENCIES)
 endif
 
 ART_TARGET_DEPENDENCIES := \
-  $(ART_TARGET_EXECUTABLES) \
-  $(ART_TARGET_DEX_DEPENDENCIES) \
-  $(ART_TARGET_SHARED_LIBRARY_DEPENDENCIES)
-
-ifeq ($(ART_BUILD_TARGET_DEBUG),true)
-ART_TARGET_DEPENDENCIES += $(ART_TARGET_SHARED_LIBRARY_DEBUG_DEPENDENCIES)
-endif
+  $(ART_TARGET_DEX_DEPENDENCIES)
 
 ########################################################################
 # test rules
@@ -532,6 +526,15 @@ PRIVATE_RUNTIME_DEPENDENCY_LIBS := \
   lib/libandroidio.so \
   lib64/libandroidio.so \
 
+# Generate copies of Bionic bootstrap artifacts and Runtime APEX
+# libraries in the `system` (TARGET_OUT) directory. This is dangerous
+# as these files could inadvertently stay in this directory and be
+# included in a system image.
+#
+# This target is only used by Golem now.
+#
+# TODO(b/121117762): Remove this when Golem has full support for the
+# Runtime APEX.
 .PHONY: standalone-apex-files
 standalone-apex-files: libc.bootstrap libdl.bootstrap libm.bootstrap linker com.android.runtime.debug
 	for f in $(PRIVATE_BIONIC_FILES); do \
