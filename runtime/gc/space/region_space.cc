36ff084601187a7e592860e557d7c4b3273e90f5
Lokesh Gidra <lokeshgidra@google.com>
2020-02-12 18:10:32 -0800

    Use MADV_FREE to reclaim pages of freed regions
    In order to release memory consumed by freed regions back to kernel we
    use madvise(MADV_DONTNEED) syscall, which synchronously takes away all
    the pages of the given memory range. As a side-effect, this also provides
    us clean (zeroed-out) pages the next time region is used by an application
    thread as a TLAB. The downside is overhead of MADV_DONTNEED (it has to
    manipulate all the corresponding page-table entries). Furthermore, the
    application thread gets page fault each time it moves from one page to
    the next while consuming the TLAB.
    
    MADV_FREE is another madvise option that also advises kernel to take
    away the pages, but only when there is memory pressure. It is a much
    more light-weight operation than MADV_DONTNEED. Also, the application
    thread doesn't get the page fault overhead, if the page is not already
    reclaimed. However, a page which is not reclaimed by the kernel will have
    stale data on it.
    
    This change uses MADV_FREE for region-space's reclamation and ensures
    that for pages which are not taken away by the kernel are cleaned by the
    application thread before it allocates objects on them.
    
    Bug:74447417
    Bug:140130889
    Test: Golem, mpts/greenday tests
    Change-Id: I1e4a75abed51844d5062685bf77871f609af5a65

--- runtime/gc/space/region_space.cc
+++ runtime/gc/space/region_space.cc
@@ -141,7 +141,7 @@ RegionSpace::RegionSpace(const std::string& name, MemMap&& mem_map, bool use_gen
   DCHECK(!full_region_.IsFree());
   DCHECK(full_region_.IsAllocated());
   size_t ignored;
-  DCHECK(full_region_.Alloc(kAlignment, &ignored, nullptr, &ignored) == nullptr);
+  DCHECK(full_region_.Alloc</*kForEvac*/true>(kAlignment, &ignored, nullptr, &ignored) == nullptr);
   // Protect the whole region space from the start.
   Protect();
 }
@@ -459,6 +459,19 @@ void RegionSpace::ClearFromSpace(/* out */ uint64_t* cleared_bytes,
         clear_block_begin = r->Begin();
       }
       clear_block_end = r->End();
+      // Mark the first byte of every page so that we can catch during
+      // TLAB allocation if the page is already reclaimed by the kernel and
+      // hence cleaned, or not.
+      {
+        uint8_t* mark_begin_page = r->Begin();
+        // Only pages up to r->Top() need to be marked.
+        uint8_t* const mark_end_page = AlignUp(r->Top(), kPageSize);
+        DCHECK(r->IsLarge() || mark_end_page <= r->End());
+        while (mark_begin_page < mark_end_page) {
+          *mark_begin_page = kMadvFreeMagic;
+          mark_begin_page += kPageSize;
+        }
+      }
     };
     for (size_t i = 0; i < std::min(num_regions_, non_free_region_index_limit_); ++i) {
       Region* r = &regions_[i];
@@ -500,7 +513,7 @@ void RegionSpace::ClearFromSpace(/* out */ uint64_t* cleared_bytes,
 
   // Madvise the memory ranges.
   for (const auto &iter : madvise_list) {
-    ZeroAndProtectRegion(iter.first, iter.second);
+    PurgePages(iter.first, iter.second - iter.first);
     if (clear_bitmap) {
       GetLiveBitmap()->ClearRange(
           reinterpret_cast<mirror::Object*>(iter.first),
@@ -867,6 +880,9 @@ bool RegionSpace::AllocNewTlab(Thread* self,
   if (r != nullptr) {
     uint8_t* start = pos != nullptr ? pos : r->Begin();
     DCHECK_ALIGNED(start, kObjectAlignment);
+    // If we are allocating a partially utilized TLAB, then the tlab is already
+    // clean from [pos, r->Top()).
+    ZeroAllocRange(pos != nullptr ? r->Top() : r->Begin(), *bytes_tl_bulk_allocated);
     r->is_a_tlab_ = true;
     r->thread_ = self;
     r->SetTop(r->End());
