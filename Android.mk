c7d9601225e8e65b36b234b6e61b992c76fc77cb
Martin Stjernholm <mast@google.com>
2021-05-14 18:15:56 +0100

    Remove libdexfile_support shared library.
    It's a tiny library that doesn't justify the DSO overhead - only
    provide it as a static library.
    
    Test: Boot
    Bug: 143978909
    Change-Id: I3d2be11e7b01dcfeafffab518833ca7425e95f4c

--- Android.mk
+++ Android.mk
@@ -505,8 +505,6 @@ PRIVATE_ART_APEX_DEPENDENCY_LIBS := \
   lib/libartpalette.so \
   lib/libart.so \
   lib/libbacktrace.so \
-  lib/libdexfile_external.so \
-  lib/libdexfile_support.so \
   lib/libdexfile.so \
   lib/libdt_fd_forward.so \
   lib/libdt_socket.so \
@@ -537,8 +535,6 @@ PRIVATE_ART_APEX_DEPENDENCY_LIBS := \
   lib64/libartpalette.so \
   lib64/libart.so \
   lib64/libbacktrace.so \
-  lib64/libdexfile_external.so \
-  lib64/libdexfile_support.so \
   lib64/libdexfile.so \
   lib64/libdt_fd_forward.so \
   lib64/libdt_socket.so \
