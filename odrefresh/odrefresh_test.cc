ecae09cf506027a2ac72e108e2ccb8b05cd03519
Jiakai Zhang <jiakaiz@google.com>
2021-08-12 13:59:56 +0000

    Add unit tests for odrefresh - step 2
    Bug: 196188549
    Test: art_standalone_odrefresh_tests
    Test: atest odsign_e2e_tests
    Change-Id: Ia7b4347bba6873878490a513b8a5ff6642122d71

--- odrefresh/odrefresh_test.cc
+++ odrefresh/odrefresh_test.cc
@@ -16,17 +16,52 @@
 
 #include "odrefresh/odrefresh.h"
 
+#include <memory>
+
 #include "base/common_art_test.h"
 #include "base/file_utils.h"
+#include "gtest/gtest.h"
+#include "odr_fs_utils.h"
 
 namespace art {
 namespace odrefresh {
 
-TEST(OdRefreshTest, OdrefreshArtifactDirectory) {
-    // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
-    // where compilation artifacts end up.
-    ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
-    EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
+constexpr int kReplace = 1;
+
+class OdRefreshTest : public CommonArtTest {
+ protected:
+  void SetUp() override {
+    CommonArtTest::SetUp();
+
+    temp_dir_ = std::make_unique<ScratchDir>();
+
+    std::string android_root_path = temp_dir_->GetPath() + "/system";
+    ASSERT_TRUE(EnsureDirectoryExists(android_root_path));
+    setenv("ANDROID_ROOT", android_root_path.c_str(), kReplace);
+
+    std::string art_apex_data_path = temp_dir_->GetPath() + kOdrefreshArtifactDirectory;
+    ASSERT_TRUE(EnsureDirectoryExists(art_apex_data_path));
+    setenv("ART_APEX_DATA", art_apex_data_path.c_str(), kReplace);
+
+    ASSERT_TRUE(EnsureDirectoryExists(art_apex_data_path + "/dalvik-cache"));
+  }
+
+  void TearDown() override {
+    temp_dir_.reset();
+
+    CommonArtTest::TearDown();
+  }
+
+  std::unique_ptr<ScratchDir> temp_dir_;
+  ScopedUnsetEnvironmentVariable android_root_env_{"ANDROID_ROOT"};
+  ScopedUnsetEnvironmentVariable art_apex_data_env_{"ART_APEX_DATA"};
+};
+
+TEST_F(OdRefreshTest, OdrefreshArtifactDirectory) {
+  // odrefresh.h defines kOdrefreshArtifactDirectory for external callers of odrefresh. This is
+  // where compilation artifacts end up.
+  ScopedUnsetEnvironmentVariable no_env("ART_APEX_DATA");
+  EXPECT_EQ(kOdrefreshArtifactDirectory, GetArtApexData() + "/dalvik-cache");
 }
 
 }  // namespace odrefresh
