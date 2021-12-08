3905a0ff5e6f3d378404a13482177b9c115eba2d
Mythri Alle <mythria@google.com>
2021-12-08 11:27:02 +0000

    Update how deoptimization from runtime methods is supported
    We need to support deoptimization when returning from runtime methods
    - To support setting break points or other debug related features for
      long running loops we should be able to deopt at suspend points.
    - To deoptimize the full stack when we have runtime functions like
      art_quick_to_interpreter_bridge on the stack.
    
    Earlier this was achieved by instrumenting the stack and updating
    the return pcs of the runtime functions to instrumentation exit
    stub, so we could deoptimize the method. However runtime methods
    can be called in different scenarios which complicated the logic
    and was inaccurate in some scenarios.
    
    This CL changes the approach to check if a deoptimization is
    required before returning from the slow path of suspend
    function. This means we might delay deoptimization till the next
    suspend point but it is safe to do so.
    
    Bug: 204766614,208587492
    Test: art/test.py
    Change-Id: Ia655d957bae4ff891f8f32a0f3110c80e7a80bfd

--- test/1995-final-virtual-structural-multithread/src/art/Test1995.java
+++ test/1995-final-virtual-structural-multithread/src/art/Test1995.java
@@ -129,8 +129,16 @@ public class Test1995 {
 
     public void Check() throws Exception {
       for (String s : results) {
+        // The original implementation returns "Hello from <thread_name>" and the new implementation
+        // returns "Hello world, Bonjour..." string. It is possible that we see intermediate null
+        // values in some cases. The Transform class declares strings and initializes them in the
+        // constructor. If we redefine the class just after construction but before we call sayHi
+        // function then the strings will be null since we don't run the constructor again. We would
+        // see "Hello, null, null, null..". If we redefine the class after calling sayHi but before
+        // loading the strings then we could see "null from".
         if (!s.equals("Hello from " + getName()) &&
             !s.equals("Hello, null, null, null from " + getName()) &&
+            !s.equals("null from " + getName()) &&
             !s.equals("Hello World, Bonjour le Monde, Hej Verden, こんにちは世界 from " + getName())) {
           System.out.println("FAIL " + thr_id + ": Unexpected result: " + s);
         }
