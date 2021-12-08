d9e7ce69cb43769cbf43cf47ebd60ab29e1119b5
Mythri Alle <mythria@google.com>
2021-12-08 11:27:02 +0000

    Update how deoptimization from runtime methods is supported
    We need to support deoptimization when returning from runtime methods
    - To support setting break points or other debug related features for
      long running loops we should be able to deopt at suspend points.
    - To deoptimize the full stack when we have runtime functions like
      art_quick_to_interpreter_bridge on the stack.
    - To deoptimize the optimized code after redefining the class. Once
      we redefine the class the offsets of the fields could change
      because the field offsets are grouped together by type when
      allocating. This would invalidate all optimized code and
      deoptimization cannot be pushed to a later point.
    
    Earlier this was achieved by instrumenting the stack and updating
    the return pcs of the runtime functions to instrumentation exit
    stub, so we could deoptimize the method. However runtime methods
    can be called in different scenarios which complicated the logic
    and was inaccurate in some scenarios.
    
    This CL changes the approach to check if a deoptimization is
    required before returning from the slow path of runtime functions
    that can suspend. Checking for deopts only at suspend points is not
    sufficient because of the class redefinition support as mentioned
    earlier.
    
    Bug: 204766614,208587492
    Test: art/test.py
    Change-Id: Ia655d957bae4ff891f8f32a0f3110c80e7a80bfd

--- runtime/entrypoints/quick/quick_thread_entrypoints.cc
+++ runtime/entrypoints/quick/quick_thread_entrypoints.cc
@@ -21,16 +21,52 @@
 
 namespace art {
 
+extern "C" void artDeoptimizeIfNeeded(Thread* self, uintptr_t result, bool is_ref)
+    REQUIRES_SHARED(Locks::mutator_lock_) {
+  instrumentation::Instrumentation* instr = Runtime::Current()->GetInstrumentation();
+  DCHECK(!self->IsExceptionPending());
+
+  ArtMethod** sp = self->GetManagedStack()->GetTopQuickFrame();
+  DCHECK(sp != nullptr);
+  // A few runtime methods like artLockObjectFromCode can be called both from native and compiled
+  // code. If it is called from native code no further checks are required.
+  if ((*sp)->IsNative()) {
+    return;
+  }
+  DCHECK((*sp)->IsRuntimeMethod());
+
+  DeoptimizationMethodType type = instr->GetDeoptimizationMethodType(*sp);
+  JValue jvalue;
+  jvalue.SetJ(result);
+  instr->DeoptimizeIfNeeded(self, sp, type, jvalue, is_ref);
+}
+
 extern "C" void artTestSuspendFromCode(Thread* self) REQUIRES_SHARED(Locks::mutator_lock_) {
   // Called when there is a pending checkpoint or suspend request.
   ScopedQuickEntrypointChecks sqec(self);
   self->CheckSuspend();
+
+  // We could have other dex instructions at the same dex pc as suspend and we need to execute
+  // those instructions. So we should start executing from the current dex pc.
+  ArtMethod** sp = self->GetManagedStack()->GetTopQuickFrame();
+  JValue result;
+  result.SetJ(0);
+  Runtime::Current()->GetInstrumentation()->DeoptimizeIfNeeded(
+      self, sp, DeoptimizationMethodType::kKeepDexPc, result, /* is_ref= */ false);
 }
 
 extern "C" void artImplicitSuspendFromCode(Thread* self) REQUIRES_SHARED(Locks::mutator_lock_) {
   // Called when there is a pending checkpoint or suspend request.
   ScopedQuickEntrypointChecks sqec(self);
   self->CheckSuspend(/*implicit=*/ true);
+
+  // We could have other dex instructions at the same dex pc as suspend and we need to execute
+  // those instructions. So we should start executing from the current dex pc.
+  ArtMethod** sp = self->GetManagedStack()->GetTopQuickFrame();
+  JValue result;
+  result.SetJ(0);
+  Runtime::Current()->GetInstrumentation()->DeoptimizeIfNeeded(
+      self, sp, DeoptimizationMethodType::kKeepDexPc, result, /* is_ref= */ false);
 }
 
 extern "C" void artCompileOptimized(ArtMethod* method, Thread* self)
