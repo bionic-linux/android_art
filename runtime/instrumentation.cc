8e455a7314d809b06ec884893dca25c418a916c5
Mythri Alle <mythria@google.com>
2022-08-01 14:31:25 +0000

    Update instrumentation support for non-java debuggable runtimes
    Support for debugger related features wasn't consistent earlier. This CL
    unifies the support for debuggable / non-debuggble runtimes and supprt
    debugger related feature more consistently in non-debuggable runtime.
    Debug features are still supported on a best effort basis for any
    methods on the stack when the debugger was attached. Certain features
    like redefinition are still not supported.
    
    This CL:
    1. Replaces is_debuggable boolean with an enum with three states:
       NonJavaDebuggable, JavaDebuggable, JavaDebuggableAtInit
    2. Adds support to switch between NonJavaDebuggable -> JavaDebuggable
       and vice versa.
    3. Updates the checks for IsJavaDebuggable to check for
       JavaDebuggableAtInit or JavaDebuggable as required.
    
    Test: art/test.py
    Bug: 206029744
    Change-Id: I1d072d8a1d215b70f3d2425b458b836a22af6812

--- runtime/instrumentation.cc
+++ runtime/instrumentation.cc
@@ -450,6 +450,14 @@ void Instrumentation::InstallStubsForMethod(ArtMethod* method) {
   UpdateEntryPoints(method, GetOptimizedCodeFor(method));
 }
 
+void Instrumentation::UpdateEntrypointsForDebuggable() {
+  Runtime* runtime = Runtime::Current();
+  // If we are transitioning from non-debuggable to debuggable, we patch
+  // entry points of methods to remove any aot / JITed entry points.
+  InstallStubsClassVisitor visitor(this);
+  runtime->GetClassLinker()->VisitClasses(&visitor);
+}
+
 // Places the instrumentation exit pc as the return PC for every quick frame. This also allows
 // deoptimization of quick frames to interpreter frames. When force_deopt is
 // true the frames have to be deoptimized. If the frame has a deoptimization
@@ -1220,6 +1228,14 @@ void Instrumentation::Undeoptimize(ArtMethod* method) {
 
   // If interpreter stubs are still needed nothing to do.
   if (InterpreterStubsInstalled()) {
+    return;
+  }
+
+  if (method->IsObsolete()) {
+    // Don't update entry points for obsolete methods. The entrypoint should
+    // have been set to InvokeObsoleteMethoStub.
+    DCHECK_EQ(method->GetEntryPointFromQuickCompiledCodePtrSize(kRuntimePointerSize),
+              GetInvokeObsoleteMethodStub());
     return;
   }
 
