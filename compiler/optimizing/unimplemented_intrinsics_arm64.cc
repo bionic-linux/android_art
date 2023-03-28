ef362be22d3751e9f1fb9477ee836f51144d155d
Santiago Aboy Solanes <solanes@google.com>
2023-03-28 11:54:01 +0100

    Inline unimplemented intrinsics
    There are intrinsics that are unimplemented i.e. we didn't
    hand-craft code for them. Allow the inliner to inline those.
    
    Since our optimizations expect InvokeVirtual, I stopped the
    de-virtualization of intrinsics. That could be re-added
    if we modify optimizations like TryReplaceStringBuilderAppend.
    
    Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
    Test: art/test/testrunner/testrunner.py --target --64 --optimizing
    Test: compiling the APK in the bug and seeing the inline
    Bug: 262585898
    Fixes: 262585898
    Change-Id: I501b69c4ffd9082ca8ffacb1cd1cd5d1ab3668a8

---
+++ compiler/optimizing/unimplemented_intrinsics_arm64.cc
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "optimizing/unimplemented_intrinsics_arm64.h"
+
+namespace art HIDDEN {
+namespace arm64 {
+
+const std::unordered_set<Intrinsics> unimplemented_intrinsics = {
+#define ADD_TO_SET(Name) Intrinsics::k##Name,
+    UNIMPLEMENTED_INTRINSIC_LIST_ARM64(ADD_TO_SET)
+#undef ADD_TO_SET
+};
+
+}  // namespace arm64
+}  // namespace art HIDDEN
---
+++ compiler/optimizing/unimplemented_intrinsics_x86_64.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ART_COMPILER_OPTIMIZING_UNIMPLEMENTED_INTRINSICS_X86_64_H_
+#define ART_COMPILER_OPTIMIZING_UNIMPLEMENTED_INTRINSICS_X86_64_H_
+
+#include <unordered_set>
+
+#include "base/macros.h"
+#include "intrinsics_enum.h"
+
+namespace art HIDDEN {
+namespace x86_64 {
+
+#define UNIMPLEMENTED_INTRINSIC_LIST_X86_64(V) \
+  V(CRC32Update)                               \
+  V(CRC32UpdateBytes)                          \
+  V(CRC32UpdateByteBuffer)                     \
+  V(FP16ToFloat)                               \
+  V(FP16ToHalf)                                \
+  V(FP16Floor)                                 \
+  V(FP16Ceil)                                  \
+  V(FP16Rint)                                  \
+  V(FP16Greater)                               \
+  V(FP16GreaterEquals)                         \
+  V(FP16Less)                                  \
+  V(FP16LessEquals)                            \
+  V(FP16Compare)                               \
+  V(FP16Min)                                   \
+  V(FP16Max)                                   \
+  V(StringStringIndexOf)                       \
+  V(StringStringIndexOfAfter)                  \
+  V(StringBufferAppend)                        \
+  V(StringBufferLength)                        \
+  V(StringBufferToString)                      \
+  V(StringBuilderAppendObject)                 \
+  V(StringBuilderAppendString)                 \
+  V(StringBuilderAppendCharSequence)           \
+  V(StringBuilderAppendCharArray)              \
+  V(StringBuilderAppendBoolean)                \
+  V(StringBuilderAppendChar)                   \
+  V(StringBuilderAppendInt)                    \
+  V(StringBuilderAppendLong)                   \
+  V(StringBuilderAppendFloat)                  \
+  V(StringBuilderAppendDouble)                 \
+  V(StringBuilderLength)                       \
+  V(StringBuilderToString)                     \
+  /* 1.8 */                                    \
+  V(UnsafeGetAndAddInt)                        \
+  V(UnsafeGetAndAddLong)                       \
+  V(UnsafeGetAndSetInt)                        \
+  V(UnsafeGetAndSetLong)                       \
+  V(UnsafeGetAndSetObject)                     \
+  V(MethodHandleInvokeExact)                   \
+  V(MethodHandleInvoke)                        \
+  /* OpenJDK 11 */                             \
+  V(JdkUnsafeGetAndAddInt)                     \
+  V(JdkUnsafeGetAndAddLong)                    \
+  V(JdkUnsafeGetAndSetInt)                     \
+  V(JdkUnsafeGetAndSetLong)                    \
+  V(JdkUnsafeGetAndSetObject)
+
+extern const std::unordered_set<Intrinsics> unimplemented_intrinsics;
+
+}  // namespace x86_64
+}  // namespace art HIDDEN
+
+#endif  // ART_COMPILER_OPTIMIZING_UNIMPLEMENTED_INTRINSICS_X86_64_H_
