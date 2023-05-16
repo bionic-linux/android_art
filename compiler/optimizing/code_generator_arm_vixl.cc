d0b2a053660430034f65136a6b20eb79039d24cb
Chun-Ku Lin <chunkulin@google.com>
2023-05-16 20:03:04 +0000

    Revert "Fix libart-compiler ReadBarrier flag init order"
    This reverts commit d7d1e20dbd905ccbce5a669f512bb8fa564c2da6.
    
    Reason for revert: DroidMonitor: Potential culprit for Bug 282980419 - verifying through ABTD before revert submission
    
    Change-Id: I2df5960ebe0b46fc25e34cf0eb9b068a5d0df266

--- compiler/optimizing/code_generator_arm_vixl.cc
+++ compiler/optimizing/code_generator_arm_vixl.cc
@@ -7675,8 +7675,9 @@ void InstructionCodeGeneratorARMVIXL::VisitLoadClass(HLoadClass* cls) NO_THREAD_
   Location out_loc = locations->Out();
   vixl32::Register out = OutputRegister(cls);
 
-  const ReadBarrierOption read_barrier_option =
-      cls->IsInBootImage() ? kWithoutReadBarrier : GetCompilerReadBarrierOption();
+  const ReadBarrierOption read_barrier_option = cls->IsInBootImage()
+      ? kWithoutReadBarrier
+      : gCompilerReadBarrierOption;
   bool generate_null_check = false;
   switch (load_kind) {
     case HLoadClass::LoadKind::kReferrersClass: {
@@ -7930,7 +7931,7 @@ void InstructionCodeGeneratorARMVIXL::VisitLoadString(HLoadString* load) NO_THRE
       codegen_->EmitMovwMovtPlaceholder(labels, out);
       // All aligned loads are implicitly atomic consume operations on ARM.
       codegen_->GenerateGcRootFieldLoad(
-          load, out_loc, out, /*offset=*/0, GetCompilerReadBarrierOption());
+          load, out_loc, out, /*offset=*/ 0, gCompilerReadBarrierOption);
       LoadStringSlowPathARMVIXL* slow_path =
           new (codegen_->GetScopedAllocator()) LoadStringSlowPathARMVIXL(load);
       codegen_->AddSlowPath(slow_path);
@@ -7951,7 +7952,7 @@ void InstructionCodeGeneratorARMVIXL::VisitLoadString(HLoadString* load) NO_THRE
                                                         load->GetString()));
       // /* GcRoot<mirror::String> */ out = *out
       codegen_->GenerateGcRootFieldLoad(
-          load, out_loc, out, /*offset=*/0, GetCompilerReadBarrierOption());
+          load, out_loc, out, /*offset=*/ 0, gCompilerReadBarrierOption);
       return;
     }
     default:
