hopez13/art

RISCV: [Codegen] Add VisitArrayLength
NOTICE: WIP
Change-Id: Icda3fe0a2d503fd1dd4c197df61abd95be78f86b

compiler/optimizing/code_generator_riscv64.cc
@@ -1208,12 +1208,21 @@ void InstructionCodeGeneratorRISCV64::VisitArrayGet(HArrayGet* instruction) {
   LOG(FATAL) << "Unimplemented";
 }
 void LocationsBuilderRISCV64::VisitArrayLength(HArrayLength* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+  LocationSummary* locations = new (GetGraph()->GetAllocator()) LocationSummary(instruction);
+  locations->SetInAt(0, Location::RequiresRegister());
+  locations->SetOut(Location::RequiresRegister(), Location::kNoOutputOverlap);
 }
 void InstructionCodeGeneratorRISCV64::VisitArrayLength(HArrayLength* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+  LocationSummary* locations = instruction->GetLocations();
+  uint32_t offset = CodeGenerator::GetArrayLengthOffset(instruction);
+  XRegister obj = locations->InAt(0).AsRegister<XRegister>();
+  XRegister out = locations->Out().AsRegister<XRegister>();
+  __ Loadw(out, obj, offset);
+  codegen_->MaybeRecordImplicitNullCheck(instruction);
+  // Mask out compression flag from String's array length.
+  if (mirror::kUseStringCompression && instruction->IsStringLength()) {
+    __ Srliw(out, out, 1u);
+  }
 }
 
 void LocationsBuilderRISCV64::VisitArraySet(HArraySet* instruction) {

bionic-linux/android_art

Test: m test-art-host-gtest
Bug: 283082089
Signed-off-by: Lifang Xia <lifang_xia@linux.alibaba.com>
Signed-off-by: Wendong Wang <wangwd@xcvmbyte.com>

compiler/optimizing/code_generator_riscv64.cc
@@ -1096,13 +1096,22 @@ void InstructionCodeGeneratorRISCV64::VisitArrayGet(HArrayGet* instruction) {
 }
 
 void LocationsBuilderRISCV64::VisitArrayLength(HArrayLength* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+  LocationSummary* locations = new (GetGraph()->GetAllocator()) LocationSummary(instruction);
+  locations->SetInAt(0, Location::RequiresRegister());
+  locations->SetOut(Location::RequiresRegister(), Location::kNoOutputOverlap);
 }
 
 void InstructionCodeGeneratorRISCV64::VisitArrayLength(HArrayLength* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+  LocationSummary* locations = instruction->GetLocations();
+  uint32_t offset = CodeGenerator::GetArrayLengthOffset(instruction);
+  XRegister obj = locations->InAt(0).AsRegister<XRegister>();
+  XRegister out = locations->Out().AsRegister<XRegister>();
+  __ Loadwu(out, obj, offset);
+  codegen_->MaybeRecordImplicitNullCheck(instruction);
+  // Mask out compression flag from String's array length.
+  if (mirror::kUseStringCompression && instruction->IsStringLength()) {
+    __ Srli(out, out, 1u);
+  }
 }
 
 void LocationsBuilderRISCV64::VisitArraySet(HArraySet* instruction) {