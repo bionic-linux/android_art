3f0477148e7cea8d8c8469f7890573bdb32de140
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-09-29 09:01:56 +0000

    Revert "Fix running uffd GC when transitioning to background state"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/92f2b0ad-d96a-48ad-8a2d-898d243e131a). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: If70affe7362bd71951b7e9b342b4138458163e75

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -428,10 +428,7 @@ Heap::Heap(size_t initial_size,
   }
 
   LOG(INFO) << "Using " << foreground_collector_type_ << " GC.";
-  if (gUseUserfaultfd) {
-    CHECK_EQ(foreground_collector_type_, kCollectorTypeCMC);
-    CHECK_EQ(background_collector_type_, kCollectorTypeCMCBackground);
-  } else {
+  if (!gUseUserfaultfd) {
     // This ensures that userfaultfd syscall is done before any seccomp filter is installed.
     // TODO(b/266731037): Remove this when we no longer need to collect metric on userfaultfd
     // support.
@@ -1569,7 +1566,7 @@ void Heap::DoPendingCollectorTransition() {
       VLOG(gc) << "Homogeneous compaction ignored due to jank perceptible process state";
     }
   } else if (desired_collector_type == kCollectorTypeCCBackground ||
-             desired_collector_type == kCollectorTypeCMCBackground) {
+             desired_collector_type == kCollectorTypeCMC) {
     if (!CareAboutPauseTimes()) {
       // Invoke full compaction.
       CollectGarbageInternal(collector::kGcTypeFull,
@@ -3992,12 +3989,7 @@ void Heap::RequestCollectorTransition(CollectorType desired_collector_type, uint
     // doesn't change.
     DCHECK_EQ(desired_collector_type_, kCollectorTypeCCBackground);
   }
-  if (collector_type_ == kCollectorTypeCMC) {
-    // For CMC collector type doesn't change.
-    DCHECK_EQ(desired_collector_type_, kCollectorTypeCMCBackground);
-  }
   DCHECK_NE(collector_type_, kCollectorTypeCCBackground);
-  DCHECK_NE(collector_type_, kCollectorTypeCMCBackground);
   CollectorTransitionTask* added_task = nullptr;
   const uint64_t target_time = NanoTime() + delta_time;
   {
