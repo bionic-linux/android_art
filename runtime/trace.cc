4599248914057b8531e68c4410eefbe9a8a55adf
Mythri Alle <mythria@google.com>
2023-11-28 13:40:58 +0000

    Buffer size - 32 * 1024 * 52
    Change-Id: I0fc0acc4c5096b76e334f1d972756344dd5f6240

--- runtime/trace.cc
+++ runtime/trace.cc
@@ -248,13 +248,16 @@ class TraceWriterTask final : public Task {
         thread_id_(thread_id) {}
 
   void Run(Thread* self ATTRIBUTE_UNUSED) override {
-    std::unordered_map<ArtMethod*, std::string> method_infos;
+    DCHECK(trace_writer_ != nullptr);
+    DCHECK_GE(thread_id_ + cur_offset_, 0);
+    DCHECK(buffer_ != nullptr);
+    /*std::unordered_map<ArtMethod*, std::string> method_infos;
     {
       ScopedObjectAccess soa(Thread::Current());
       trace_writer_->PreProcessTraceForMethodInfos(buffer_, cur_offset_, method_infos);
     }
-    trace_writer_->FlushBuffer(buffer_, cur_offset_, thread_id_, method_infos);
-    delete[] buffer_;
+    trace_writer_->FlushBuffer(buffer_, cur_offset_, thread_id_, method_infos);*/
+    // delete[] buffer_;
   }
 
  private:
@@ -1192,9 +1195,9 @@ void TraceWriter::FlushBuffer(Thread* thread, bool is_sync) {
   DCHECK(method_trace_entries != nullptr);
 
   if (is_sync || thread_pool_ == nullptr) {
-    std::unordered_map<ArtMethod*, std::string> method_infos;
+    /*std::unordered_map<ArtMethod*, std::string> method_infos;
     PreProcessTraceForMethodInfos(method_trace_entries, *current_offset, method_infos);
-    FlushBuffer(method_trace_entries, *current_offset, tid, method_infos);
+    FlushBuffer(method_trace_entries, *current_offset, tid, method_infos);*/
 
     // This is a synchronous flush, so no need to allocate a new buffer. This is used either
     // when the tracing has finished or in non-streaming mode.
@@ -1208,8 +1211,8 @@ void TraceWriter::FlushBuffer(Thread* thread, bool is_sync) {
 
     // Create a new buffer and update the per-thread buffer so we don't have to wait for the
     // flushing to finish.
-    uintptr_t* method_trace_buffer = new uintptr_t[std::max(kMinBufSize, kPerThreadBufSize)]();
-    thread->SetMethodTraceBuffer(method_trace_buffer);
+    // uintptr_t* method_trace_buffer = new uintptr_t[std::max(kMinBufSize, kPerThreadBufSize)]();
+    // thread->SetMethodTraceBuffer(method_trace_buffer);
     *current_offset = kPerThreadBufSize;
   }
 
