9ac81a39fc7380c9f593ad69cbe89a0055345ac3
Nicolas Geoffray <ngeoffray@google.com>
2024-07-15 14:02:30 +0100

    Test case for b/351868772
    Change-Id: I78ba796c54805ebae029c016b2fe493c5de4acd3

---
+++ test/858-jit-crash/src/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  void a() {
+    int b, f;
+    for (int c = -151; c < 5; c += 3) {
+      byte ab = 3;
+      short i = 1;
+      for (int d = 1; d < 5; d++)
+        ab = 1;
+      for (int e = 7; e < 124; ++e)
+        i += ab;
+      long g = i;
+      f = (int)g;
+      b = 4 / f;
+    }
+  }
+  public static void main(String[] h) {
+    Main aa = new Main();
+    aa.a();
+  }
+}
