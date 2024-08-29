2a8929248f01ae1812a952192aa1ea7e53a51fa0
Santiago Aboy Solanes <solanes@google.com>
2024-08-29 14:23:04 +0100

    cleanup: change Set/GetIntrinsic in ArtMethod to use Intrinsics
    Change-Id: I253a6bfe6bba7e02e527722c4632cb60938fe1c6

--- compiler/optimizing/inliner.cc
+++ compiler/optimizing/inliner.cc
@@ -1344,8 +1344,7 @@ bool HInliner::TryDevirtualize(HInvoke* invoke_instruction,
   }
 
   // Don't devirtualize to an intrinsic invalid after the builder phase.
-  if (method->IsIntrinsic() &&
-      !IsValidIntrinsicAfterBuilder(static_cast<Intrinsics>(method->GetIntrinsic()))) {
+  if (method->IsIntrinsic() && !IsValidIntrinsicAfterBuilder(method->GetIntrinsic())) {
     return false;
   }
 
@@ -1596,7 +1595,7 @@ bool HInliner::TryBuildAndInline(HInvoke* invoke_instruction,
   // another chance before we try to inline it.
   if (invoke_instruction->GetResolvedMethod() != method &&
       method->IsIntrinsic() &&
-      IsValidIntrinsicAfterBuilder(static_cast<Intrinsics>(method->GetIntrinsic()))) {
+      IsValidIntrinsicAfterBuilder(method->GetIntrinsic())) {
     MaybeRecordStat(stats_, MethodCompilationStat::kIntrinsicRecognized);
     // For simplicity, always create a new instruction to replace the existing
     // invoke.
