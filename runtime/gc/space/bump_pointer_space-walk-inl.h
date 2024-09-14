09faaf9a3e5507f3a16fbcda28a160313f99408e
Lokesh Gidra <lokeshgidra@google.com>
2024-09-14 00:47:00 +0000

    Reland "Introduce black-dense region in moving space"
    This reverts commit b166ff7065f3c0f002ee97fe7c8f89d57ef9c0d9.
    
    Reason for revert: Fix Walk logic in BumpPointerSpace to take
    care of case where black-dense region is bigger than the main-block
    
    Bug: 365657433
    Bug: 343220989
    Change-Id: Iab9776270f460db1a2b17f619c414b45893444b1
    Test: art/test/testrunner/testrunner.py --host
    Test: atest CtsLibcoreTestCases (on cuttlefish)

--- runtime/gc/space/bump_pointer_space-walk-inl.h
+++ runtime/gc/space/bump_pointer_space-walk-inl.h
@@ -34,6 +34,7 @@ inline void BumpPointerSpace::Walk(Visitor&& visitor) {
   uint8_t* pos = Begin();
   uint8_t* end = End();
   uint8_t* main_end = pos;
+  size_t black_dense_size;
   std::unique_ptr<std::vector<size_t>> block_sizes_copy;
   // Internal indirection w/ NO_THREAD_SAFETY_ANALYSIS. Optimally, we'd like to have an annotation
   // like
@@ -64,6 +65,30 @@ inline void BumpPointerSpace::Walk(Visitor&& visitor) {
     } else {
       block_sizes_copy.reset(new std::vector<size_t>(block_sizes_.begin(), block_sizes_.end()));
     }
+
+    black_dense_size = black_dense_region_size_;
+  }
+
+  // black_dense_region_size_ will be non-zero only in case of moving-space of CMC GC.
+  if (black_dense_size > 0) {
+    // Objects are not packed in this case, and therefore the bitmap is needed
+    // to walk this part of the space.
+    // Remember the last object visited using bitmap to be able to fetch its size.
+    mirror::Object* last_obj = nullptr;
+    auto return_obj_visit = [&](mirror::Object* obj) NO_THREAD_SAFETY_ANALYSIS {
+      visitor(obj);
+      last_obj = obj;
+    };
+    GetMarkBitmap()->VisitMarkedRange(reinterpret_cast<uintptr_t>(pos),
+                                      reinterpret_cast<uintptr_t>(pos + black_dense_size),
+                                      return_obj_visit);
+    pos += black_dense_size;
+    if (last_obj != nullptr) {
+      // If the last object visited using bitmap was large enough to go past the
+      // black-dense region, then we need to adjust for that to be able to visit
+      // objects one after the other below.
+      pos = std::max(pos, reinterpret_cast<uint8_t*>(GetNextObject(last_obj)));
+    }
   }
   // Walk all of the objects in the main block first.
   while (pos < main_end) {
@@ -83,7 +108,14 @@ inline void BumpPointerSpace::Walk(Visitor&& visitor) {
   if (block_sizes_copy != nullptr) {
     for (size_t block_size : *block_sizes_copy) {
       mirror::Object* obj = reinterpret_cast<mirror::Object*>(pos);
-      const mirror::Object* end_obj = reinterpret_cast<const mirror::Object*>(pos + block_size);
+      // Adjust for the case where 'pos' has gone past main-block due to black-dense region.
+      main_end += block_size;
+      if (main_end <= pos) {
+        DCHECK_GT(black_dense_size, 0u);
+        continue;
+      }
+      pos = main_end;
+      const mirror::Object* end_obj = reinterpret_cast<const mirror::Object*>(pos);
       CHECK_LE(reinterpret_cast<const uint8_t*>(end_obj), End());
       // We don't know how many objects are allocated in the current block. When we hit a null class
       // assume it's the end. TODO: Have a thread update the header when it flushes the block?
@@ -92,7 +124,6 @@ inline void BumpPointerSpace::Walk(Visitor&& visitor) {
         no_thread_safety_analysis_visit(obj);
         obj = GetNextObject(obj);
       }
-      pos += block_size;
     }
   } else {
     CHECK_EQ(end, main_end);
