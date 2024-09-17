a5695ed2f0865fb40396fe767040c4f95d873ded
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-09-17 14:06:50 +0000

    Revert "Fix the offset discrepancy for --dump-method-and-offset-..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/c928a75a-9a6d-44bb-80ff-efb73dd20e2b). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Bug: 367481780
    Change-Id: I7fb8f95f286df37d2cb67496fd3d021bb9d00f3a
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- oatdump/oatdump.cc
+++ oatdump/oatdump.cc
@@ -721,9 +721,8 @@ class OatDumper {
 
           std::string pretty_method = dex_file->PrettyMethod(dex_method_idx, true);
 
-          os << StringPrintf("{\"method\":\"%s\",\"offset\":\"0x%08zx\"}\n",
-                             pretty_method.c_str(),
-                             AdjustOffset(code_offset));
+          os << StringPrintf(
+              "{\"method\":\"%s\",\"offset\":\"0x%08x\"}\n", pretty_method.c_str(), code_offset);
         }
       }
     }
