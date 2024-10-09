f05eca59799c9672e9b03edccae0378a1c8c3721
Vladimir Marko <vmarko@google.com>
2024-10-09 17:17:47 +0200

    Refactor `ReferenceTypeInfo` out of `nodes.{h,cc}`.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Change-Id: I0c2f310606bb03f264038534c23f15dd0fee5662

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -3135,18 +3135,6 @@ void HBoundType::SetUpperBound(const ReferenceTypeInfo& upper_bound, bool can_be
   SetPackedFlag<kFlagUpperCanBeNull>(can_be_null);
 }
 
-ReferenceTypeInfo ReferenceTypeInfo::Create(TypeHandle type_handle, bool is_exact) {
-  if (kIsDebugBuild) {
-    ScopedObjectAccess soa(Thread::Current());
-    DCHECK(IsValidHandle(type_handle));
-    if (!is_exact) {
-      DCHECK(!type_handle->CannotBeAssignedFromOtherTypes())
-          << "Callers of ReferenceTypeInfo::Create should ensure is_exact is properly computed";
-    }
-  }
-  return ReferenceTypeInfo(type_handle, is_exact);
-}
-
 std::ostream& operator<<(std::ostream& os, const ReferenceTypeInfo& rhs) {
   ScopedObjectAccess soa(Thread::Current());
   os << "["
