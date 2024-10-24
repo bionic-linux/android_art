0cba4c6fb6166806f83fcf701fa41ebb513f7bb8
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-10-24 13:33:39 +0000

    Revert "Delete compact_dex_level.h."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/eb8777cc-b460-42f5-8b43-9ae2ea600cc9). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ied4c7dd6bd48d289876e211448f293cb491d627d
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- libartbase/base/common_art_test.h
+++ libartbase/base/common_art_test.h
@@ -35,6 +35,7 @@
 #include "base/unix_file/fd_file.h"
 #include "dex/art_dex_file_loader.h"
 #include "dex/compact_dex_file.h"
+#include "dex/compact_dex_level.h"
 #include "gtest/gtest.h"
 
 namespace art {
