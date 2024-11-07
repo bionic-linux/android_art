b2c6fd5f248d321848d855194b683bf8deb81191
Almaz Mingaleev <mingaleev@google.com>
2024-11-07 10:01:49 +0000

    Remove now unnecessary checks from invokeExact intrinsic.
    Also rewrote tests to make sure that MethodHandle-s created via
    const-method-handle and Java API behave identically. They can
    be slightly different even if lookup is done in the same class
    for the same method as const-method-handle can target copied
    methods.
    
    Bug: 297147201
    Test: ./art/test/testrunner/testrunner.py -b --host --64
    Test: ./art/test/testrunner/testrunner.py -b --jvm
    
    Change-Id: I69cdf2c44f2838ee0e57eaa2e93a6ed5276c39fb

--- test/2277-methodhandle-invokeexact/build.py
+++ test/2277-methodhandle-invokeexact/build.py
@@ -1,4 +1,6 @@
 def build(ctx):
+  ctx.bash("./generate-sources")
   # To allow private interface methods.
-  ctx.default_build(javac_source_arg="17",
+  ctx.default_build(api_level="const-method-type",
+                    javac_source_arg="17",
                     javac_target_arg="17")
