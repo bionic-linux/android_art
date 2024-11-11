43700c192fd56523d0737e670a4d9651baeb5efe
Santiago Aboy Solanes <solanes@google.com>
2024-11-11 15:06:50 +0000

    Add an optimization pass that pulls common code to predecessors
    It pulls common code from both HIf branches into the parent
    HBasicBlock, as long as the instructions are the same and can
    be moved.
    
    Main benefits:
     * Less code as we deduplicate instructions
     * Allows other optimizations to take advantage of the instruction
       in a common ancestor. GVN, BCE, and LSE to name a few.
     * Helps the branch predictor as the branches will contain less
       instrutions to process.
     * Two of the most common deduplication cases are LoadClass and
       ClinitCheck which lets us emit less slow path code too.
    
    From local testing, it improves ~0.1% for speed compiles. Even if
    this is a new compiler phase, the compile time virtually doesn't
    increase. Local sampling gives ~0.05% more compile time.
    
    Test: art/test/testrunner/testrunner.py --host --64 -b --optimizing
    Change-Id: I23b13943b087a3ffd91dcab5ed55513355ca6b40

---
+++ test/2284-checker-pull-common-code/src/Main.java
@@ -0,0 +1,261 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class TestClass {
+    TestClass(int value) {
+        this.i = value;
+    }
+
+    int i;
+}
+
+// TODO(solanes): Update the CHECKs
+
+public class Main {
+    public static void main(String[] args) {
+        $noinline$testClinit(true);
+
+        $noinline$assertIntEquals(4, $noinline$testArray(new int[] {1, 2, 3}, 0, 4, false));
+        $noinline$assertIntEquals(0, $noinline$testArray(new int[] {1, 2, 3}, 0, 4, true));
+
+        $noinline$assertIntEquals(12, $noinline$testInstanceFieldGet(new TestClass(10), false));
+        $noinline$assertIntEquals(11, $noinline$testInstanceFieldGet(new TestClass(10), true));
+
+        $noinline$assertIntEquals(11, $noinline$testNestedIfs(new TestClass(10), true, true));
+        $noinline$assertIntEquals(12, $noinline$testNestedIfs(new TestClass(10), true, false));
+        $noinline$assertIntEquals(13, $noinline$testNestedIfs(new TestClass(10), false, true));
+        $noinline$assertIntEquals(14, $noinline$testNestedIfs(new TestClass(10), false, false));
+    }
+
+    static class ClassWithClinit {
+        static {
+            System.out.println("Main$ClassWithClinit's static initializer");
+        }
+
+        static void $noinline$foo() {}
+        static void $noinline$bar() {}
+    }
+
+    // TODO(solanes): Do a case there the LoadClass cannot throw.
+
+    // We can pull loading the class and clinit checks, even when the actual methods we are calling
+    // are different.
+    /// CHECK-START: void Main.$noinline$testClinit(boolean) code_pulling (before)
+    /// CHECK: LoadClass
+    /// CHECK: ClinitCheck
+    /// CHECK: LoadClass
+    /// CHECK: ClinitCheck
+
+    /// CHECK-START: void Main.$noinline$testClinit(boolean) code_pulling (after)
+    /// CHECK:     LoadClass
+    /// CHECK:     LoadClass
+
+    /// CHECK-START: void Main.$noinline$testClinit(boolean) code_pulling (after)
+    /// CHECK:     ClinitCheck
+    /// CHECK:     ClinitCheck
+    private static void $noinline$testClinit(boolean b) {
+        if (b) {
+            ClassWithClinit.$noinline$foo();
+        } else {
+            ClassWithClinit.$noinline$bar();
+        }
+    }
+
+    // We can pull null checks, array length and bounds checks. We have it thrice, but we can pull
+    // and deduplicate two. This also lets GVN remove the third.
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) code_pulling (before)
+    /// CHECK: NullCheck
+    /// CHECK: ArrayLength
+    /// CHECK: BoundsCheck
+    /// CHECK: NullCheck
+    /// CHECK: ArrayLength
+    /// CHECK: BoundsCheck
+    /// CHECK: NullCheck
+    /// CHECK: ArrayLength
+    /// CHECK: BoundsCheck
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) code_pulling (after)
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) code_pulling (after)
+    /// CHECK:     ArrayLength
+    /// CHECK:     ArrayLength
+    /// CHECK-NOT: ArrayLength
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) code_pulling (after)
+    /// CHECK:     BoundsCheck
+    /// CHECK:     BoundsCheck
+    /// CHECK-NOT: BoundsCheck
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) GVN (after)
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) GVN (after)
+    /// CHECK:     ArrayLength
+    /// CHECK-NOT: ArrayLength
+
+    /// CHECK-START: int Main.$noinline$testArray(int[], int, int, boolean) GVN (after)
+    /// CHECK:     BoundsCheck
+    /// CHECK-NOT: BoundsCheck
+    private static int $noinline$testArray(int[] arr, int index, int value, boolean b) {
+        if (b) {
+            arr[index] = 0;
+        } else {
+            arr[index] = value;
+        }
+        return arr[index];
+    }
+
+    // We can pull both a NullCheck and an InstanceFieldGet. This also lets GVN take care of the
+    // remaining NullCheck instructions. Similarly, LSE can remove the remaining InstanceFieldGet
+    // instructions.
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) code_pulling (before)
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) code_pulling (after)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) code_pulling (after)
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) GVN (before)
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) GVN (after)
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) load_store_elimination (before)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testInstanceFieldGet(TestClass, boolean) load_store_elimination (after)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+    static int $noinline$testInstanceFieldGet(TestClass obj, boolean b) {
+        if (b) {
+            obj.i += 1;
+        } else {
+            obj.i += 2;
+        }
+        return obj.i;
+    }
+
+    // Even when nested we can remove three NullCheck instructions, and GVN can remove the rest.
+    // Similarly, LSE can remove the remaining InstanceFieldGet instructions.
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) code_pulling (before)
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldSet
+    /// CHECK: NullCheck
+    /// CHECK: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) code_pulling (after)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) code_pulling (after)
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) GVN (before)
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) GVN (after)
+    /// CHECK:     NullCheck
+    /// CHECK-NOT: NullCheck
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) load_store_elimination (before)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+
+    /// CHECK-START: int Main.$noinline$testNestedIfs(TestClass, boolean, boolean) load_store_elimination (after)
+    /// CHECK:     InstanceFieldGet
+    /// CHECK-NOT: InstanceFieldGet
+    static int $noinline$testNestedIfs(TestClass obj, boolean b1, boolean b2) {
+        if (b1) {
+            if (b2) {
+                obj.i += 1;
+            } else {
+                obj.i += 2;
+            }
+        } else {
+            if (b2) {
+                obj.i += 3;
+            } else {
+                obj.i += 4;
+            }
+        }
+        return obj.i;
+    }
+
+    public static void $noinline$assertIntEquals(int expected, int result) {
+        if (expected != result) {
+            throw new Error("Expected: " + expected + ", found: " + result);
+        }
+    }
+}
