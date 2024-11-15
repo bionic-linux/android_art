db96e202be09f46d080de5e39b1781308a49cb13
Jiakai Zhang <jiakaiz@google.com>
2024-11-15 17:42:47 +0000

    Force 4K ELF alignment on art/odex files.
    Bug: 376814207
    Test: atest art_standalone_dexpreopt_tests (on 4K page size)
    Test: atest CtsCompilationTestCases (on 4K page size)
    Test: adb shell pm art dump (on 4K page size, see "speed-profile" on
      some apps, meaning odex files are accepted)
    Test: adb shell pm art dump (on 16K page size, see "[reason=vdex]" on
      all apps, meaning odex files are rejected)
    Change-Id: Ie71b8910fd9662db01f860441bc50967ce31cd0c

--- runtime/gc/space/image_space.cc
+++ runtime/gc/space/image_space.cc
@@ -65,6 +65,7 @@
 #include "oat/oat_file.h"
 #include "profile/profile_compilation_info.h"
 #include "runtime.h"
+#include "runtime_globals.h"
 #include "space-inl.h"
 
 namespace art HIDDEN {
@@ -3242,6 +3243,11 @@ bool ImageSpace::BootImageLoader::LoadFromSystem(
     /*out*/MemMap* extra_reservation,
     /*out*/std::string* error_msg) {
   TimingLogger logger(__PRETTY_FUNCTION__, /*precise=*/ true, VLOG_IS_ON(image));
+
+  if (gPageSize != kMinPageSize) {
+    *error_msg = "Loading boot image is only supported on devices with 4K page size";
+    return false;
+  }
 
   BootImageLayout layout(image_locations_,
                          boot_class_path_,
