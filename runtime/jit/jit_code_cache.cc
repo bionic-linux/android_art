ecbbfe8181b74c221f5686c64464973bb18ec142
Almaz Mingaleev <mingaleev@google.com>
2024-11-26 10:40:13 +0000

    Revert^2 "arm64: Store resolved MethodType-s in .bss."
    This reverts commit fad4678f3ae48d84b7ed1c842b03a023e4f2cb37.
    
    Bug: 380651440
    bug: 297147201
    
    Test: atest CtsLibcoreOjTestCases on a redfin device flashed
    Test: with ab/12108082: test crashes w/o WriteBarrier line
    Test: and passes w/ it.
    
    Change-Id: Ibdfc090e3c2b693c1bb3b160a64c9f94448e18ec

--- runtime/jit/jit_code_cache.cc
+++ runtime/jit/jit_code_cache.cc
@@ -806,6 +806,10 @@ bool JitCodeCache::Commit(Thread* self,
 
             DCHECK(std::find(code_ptrs.begin(), code_ptrs.end(), code_ptr) == code_ptrs.end());
             it->second.emplace_back(code_ptr);
+
+            // Unlike `j.l.Class` and `j.l.String`, `MethodType`s are strong GC roots and need
+            // write barrier.
+            WriteBarrier::ForEveryFieldWrite(method->GetDeclaringClass<kWithoutReadBarrier>());
             break;
           }
         }
